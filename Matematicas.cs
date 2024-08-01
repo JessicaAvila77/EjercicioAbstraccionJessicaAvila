d4068b692bb131890e2a2118aa8fc3e3fe8abec9
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-07-31 21:10:08 -0600

    Agregando la clase abstracta

---
+++ Matematicas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioAbstraccionJessicaAvila
+{
+    public abstract class Matematicas
+    {
+        //Métodos abstractos//
+        
+        public abstract double Potencia();
+
+        public abstract double Logaritmo();
+
+        public abstract double AreaCuadrado();
+
+        public abstract double PerimentroCuadrado();
+
+        public abstract void Imprimir();
+
+
+
+    }
+}
